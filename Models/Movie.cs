f0392ecac9941a5c145cdb9c9af9418afb1d79c6
TT-World <tetelosilas@gmail.com>
2023-02-12 11:20:21 +0200

    Add Search by genre

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -8,10 +10,12 @@ public class Movie
 
     public string? Title { get; set; }
 
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 
     public string? Genre { get; set; }
 
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
